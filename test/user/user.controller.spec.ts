c5d930fa1bdc81977cedd92f8ce6d9ab286de909
Melody Leonard <melodyleonard7@gmail.com>
2026-02-25 09:02:58 -0500

    chore(jwt): Added test for Jwt passport changes

--- test/user/user.controller.spec.ts
+++ test/user/user.controller.spec.ts
@@ -4,14 +4,23 @@ import { UserService } from '../../src/user/user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            findById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
--- test/user/user.service.spec.ts
+++ test/user/user.service.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../../src/user/user.service';
+import { UserRepository } from '../../src/user/user.repository';
 
 describe('UserService', () => {
   let service: UserService;
+  let repository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useValue: {
+            findById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repository = module.get<UserRepository>(UserRepository);
   });
 
   it('should be defined', () => {
